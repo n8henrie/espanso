4113cbde87a7763b666dfecabd0638e3e06a63c4
Nathan Henrie <nate@n8henrie.com>
2025-04-20 12:30:28 -0600

    cleanup

--- scripts/publish_homebrew_version.sh
+++ scripts/publish_homebrew_version.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+set -Eeuf -o pipefail
+set -x
 
-if [[ -z "$VERSION" ]]; then
+if [[ -z "${VERSION}" ]]; then
   echo "Missing target VERSION environment variable, please specify it"
   exit 1
 fi
@@ -13,14 +14,14 @@ VERSION=${VERSION#"v"}
 rm -Rf target/homebrew
 mkdir -p target/homebrew/artifacts
 
-echo "Targeting version $VERSION"
+echo "Targeting version ${VERSION}"
 echo "Downloading macOS artifacts"
 
-gh release download v$VERSION --pattern "Espanso-Mac*" --dir target/homebrew/artifacts
+gh release download v"${VERSION}" --pattern "Espanso-Mac*" --dir target/homebrew/artifacts
 
 echo "Reading artifacts hashes"
-INTEL_SHA=$(cat target/homebrew/artifacts/Espanso-Mac-Intel.zip.sha256.txt | awk -F ' ' '{print $1}')
-M1_SHA=$(cat target/homebrew/artifacts/Espanso-Mac-M1.zip.sha256.txt | awk -F ' ' '{print $1}')
+INTEL_SHA=$(awk -F ' ' '{print $1}' target/homebrew/artifacts/Espanso-Mac-Intel.zip.sha256.txt)
+M1_SHA=$(awk -F ' ' '{print $1}' target/homebrew/artifacts/Espanso-Mac-M1.zip.sha256.txt)
 
 echo "Cloning tap repository"
 
@@ -30,12 +31,14 @@ git clone git@github.com:espanso/homebrew-espanso.git
 pushd homebrew-espanso
 echo "Rendering formula template"
 
-cat ../../../scripts/resources/macos/formula_template.rb | sed "s/{{{VERSION}}}/$VERSION/g" | \
-  sed "s/{{{INTEL_SHA}}}/$INTEL_SHA/g" | sed "s/{{{M1_SHA}}}/$M1_SHA/g" > ./Casks/espanso.rb
+sed < ../../../scripts/resources/macos/formula_template.rb \
+  -e "s/{{{VERSION}}}/${VERSION}/g" \
+  -e "s/{{{INTEL_SHA}}}/${INTEL_SHA}/g" \
+  -e "s/{{{M1_SHA}}}/${M1_SHA}/g" > ./Casks/espanso.rb
 
 echo "Committing version update"
 git add Casks/espanso.rb
-git commit -m "Version bump: $VERSION"
+git commit -m "Version bump: ${VERSION}"
 
 echo "Pushing changes"
 git push
