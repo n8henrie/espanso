970e1d1d852572bac46ce8a1a51c32bc882dd9fe
Nathan Henrie <nate@n8henrie.com>
2025-04-20 13:19:23 -0600

    Unify macos build process, provide universal app

--- scripts/publish_homebrew_version.sh
+++ scripts/publish_homebrew_version.sh
@@ -3,44 +3,46 @@
 set -Eeuf -o pipefail
 set -x
 
-if [[ -z "${VERSION}" ]]; then
-  echo "Missing target VERSION environment variable, please specify it"
-  exit 1
-fi
+main() {
+  if [[ -z "${VERSION}" ]]; then
+    echo "Missing target VERSION environment variable, please specify it"
+    exit 1
+  fi
 
-# Removing the v suffix, if present
-VERSION=${VERSION#"v"}
+  # Removing the v suffix, if present
+  local VERSION=${VERSION#"v"}
 
-rm -Rf target/homebrew
-mkdir -p target/homebrew/artifacts
+  rm -rf target/homebrew
+  mkdir -p target/homebrew/artifacts
 
-echo "Targeting version ${VERSION}"
-echo "Downloading macOS artifacts"
+  echo "Targeting version ${VERSION}"
+  echo "Downloading macOS artifacts"
 
-gh release download v"${VERSION}" --pattern "Espanso-Mac*" --dir target/homebrew/artifacts
+  gh release download v"${VERSION}" --pattern "Espanso-Mac*" --dir target/homebrew/artifacts
 
-echo "Reading artifacts hashes"
-INTEL_SHA=$(awk -F ' ' '{print $1}' target/homebrew/artifacts/Espanso-Mac-Intel.zip.sha256.txt)
-M1_SHA=$(awk -F ' ' '{print $1}' target/homebrew/artifacts/Espanso-Mac-M1.zip.sha256.txt)
+  echo "Reading artifacts hashes"
+  SHA256=$(awk -F ' ' '{print $1}' target/homebrew/artifacts/Espanso-Mac-Universal.zip.sha256.txt)
 
-echo "Cloning tap repository"
+  echo "Cloning tap repository"
 
-pushd target/homebrew
-git clone git@github.com:espanso/homebrew-espanso.git
+  pushd target/homebrew
+  git clone git@github.com:espanso/homebrew-espanso.git
 
-pushd homebrew-espanso
-echo "Rendering formula template"
+  pushd homebrew-espanso
+  echo "Rendering formula template"
 
-sed < ../../../scripts/resources/macos/formula_template.rb \
-  -e "s/{{{VERSION}}}/${VERSION}/g" \
-  -e "s/{{{INTEL_SHA}}}/${INTEL_SHA}/g" \
-  -e "s/{{{M1_SHA}}}/${M1_SHA}/g" > ./Casks/espanso.rb
+  sed < ../../../scripts/resources/macos/formula_template.rb \
+    -e "s/{{{VERSION}}}/${VERSION}/g" \
+    -e "s/{{{SHA256}}}/${SHA256}/g" \
+    > ./Casks/espanso.rb
 
-echo "Committing version update"
-git add Casks/espanso.rb
-git commit -m "Version bump: ${VERSION}"
+  echo "Committing version update"
+  git add Casks/espanso.rb
+  git commit -m "Version bump: ${VERSION}"
 
-echo "Pushing changes"
-git push
+  echo "Pushing changes"
+  git push
 
-echo "Done!"
+  echo "Done!"
+}
+main "$@"
